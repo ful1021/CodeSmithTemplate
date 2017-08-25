006eada3df2b5d58c84501185c080c1725002ff7
付亮 <fuliang@jk724.com>
2017-08-25 11:31:33 +0800

    改名DllFile 为 AssemblyFile

--- src/CodeSmithTemplate/AspNet/AssemblyFile/Helper/CSComment.cs
+++ src/CodeSmithTemplate/AspNet/AssemblyFile/Helper/CSComment.cs
@@ -5,7 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Xml.Serialization;
 
-namespace CodeSmithTemplate
+namespace CodeSmithTemplate.AspNet.AssemblyFile
 {
     /// <remarks/>
     [SerializableAttribute()]
